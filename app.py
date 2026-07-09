b26db1e0e83a3852ede7def903aac5f5d2eb82a4
Jothi2026“ <lakshmijothi8@gmail.com>
2026-07-09 11:40:52 +0530

    fourth commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("👋 Hello, Streamlit!")
+
+st.write("Click a button below:")
+
+if st.button("Say Hello"):
+    st.success("Hello, World! 🌍")
+
+if st.button("Say Goodbye"):
+    st.warning("Goodbye! 👋")
